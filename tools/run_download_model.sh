a5a45e447eb55c0590c95f869ea19ba752c01a78
hewu2008 <hewu2008@gmail.com>
2026-08-18 15:11:38 +0800

    chore(tools): 优化模型下载脚本，使用环境变量管理路径
    将模型和夹具配置的本地存储路径改为通过环境变量配置，同时新增下载夹具描述数据集的步骤

--- tools/run_download_model.sh
+++ tools/run_download_model.sh
@@ -2,7 +2,15 @@
 
 export HF_ENDPOINT=https://hf-mirror.com
 
+export GRASPGENX_CHECKPOINT_DIR=/home/robot/hewu/model_zoo/GraspGenXModel
+export GRASPGENX_GRIPPER_CFG_DIR=/home/robot/hewu/model_zoo/gripper_descriptions
+
 hf download \
     --repo-type model \
     --repo-id adithyamurali/GraspGenXModel \
-    --local-dir /home/robot/hewu/model_zoo/GraspGenXModel
+    --local-dir $GRASPGENX_CHECKPOINT_DIR
+
+hf download \
+    --repo-type dataset \
+    --repo-id adithyamurali/gripper_descriptions \
+    --local-dir $GRASPGENX_GRIPPER_CFG_DIR
