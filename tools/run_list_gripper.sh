8c3f68cf1f3bb17f2537e5c9f00caa6392170452
hewu2008 <hewu2008@gmail.com>
2026-08-18 15:37:56 +0800

    chore(tools): add run_list_gripper.sh script
    该脚本用于配置模型和夹具描述文件目录，并执行list_grippers.py脚本列出可用夹具

---
+++ tools/run_list_gripper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export GRASPGENX_CHECKPOINT_DIR=/home/robot/hewu/model_zoo/GraspGenXModel
+export GRASPGENX_GRIPPER_CFG_DIR=/home/robot/hewu/model_zoo/gripper_descriptions
+
+python scripts/list_grippers.py
