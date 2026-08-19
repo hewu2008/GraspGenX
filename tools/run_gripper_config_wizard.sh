9ed0470e03c1bd3158f7b868e0a944592a41d415
hewu2008 <hewu2008@gmail.com>
2026-08-19 09:07:17 +0800

    build: 更新配置向导脚本并添加泽瑞特双 gripper URDF
    调整了运行脚本的默认启动参数，注释掉原有的sharpaw_right配置，新增启动左侧泽瑞特夹爪配置向导，同时新增左右夹爪的URDF资源文件

--- tools/run_gripper_config_wizard.sh
+++ tools/run_gripper_config_wizard.sh
@@ -3,12 +3,12 @@
 export GRASPGENX_CHECKPOINT_DIR=/home/robot/hewu/model_zoo/GraspGenXModel
 export GRASPGENX_GRIPPER_CFG_DIR=/home/robot/hewu/model_zoo/gripper_descriptions
 
-# python scripts/gripper_config_wizard.py \
-#     --urdf /home/robot/hewu/alg-product/Zerith_Model/ZERITH_H1_PRO_URDF/urdf/ZR_H1PRO-1.2.00.H.V4.3_URDF_2025.12.02.urdf \
-#     --name zerith_left_gripper \
-#     --port 8081
-
 python scripts/gripper_config_wizard.py \
-    --urdf $GRASPGENX_GRIPPER_CFG_DIR/gripper_descriptions/assets/x_grippers/sharpa_wave/gripper.urdf \
-    --name sharpa_right \
+    --urdf ./assets/zerith/urdf/left_gripper.urdf \
+    --name zerith_left_gripper \
     --port 8081
+
+# python scripts/gripper_config_wizard.py \
+#     --urdf $GRASPGENX_GRIPPER_CFG_DIR/gripper_descriptions/assets/x_grippers/sharpa_wave/gripper.urdf \
+#     --name sharpa_right \
+#     --port 8081
