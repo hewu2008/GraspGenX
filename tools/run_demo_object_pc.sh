a511aff49ba3f4b9f2f1f70405c6e8abfbf5ecf6
hewu2008 <hewu2008@gmail.com>
2026-08-19 10:47:32 +0800

    chore(tools): 给demo脚本添加可自定义夹爪参数的功能
    将硬编码的夹爪名改为通过脚本第一入参指定，默认使用robotiq_2f_85，同时修正了变量引用的引号格式

--- tools/run_demo_object_pc.sh
+++ tools/run_demo_object_pc.sh
@@ -3,7 +3,9 @@
 export GRASPGENX_CHECKPOINT_DIR=/home/robot/hewu/model_zoo/GraspGenXModel
 export GRASPGENX_GRIPPER_CFG_DIR=/home/robot/hewu/model_zoo/gripper_descriptions
 
+GRIPPER_NAME=${1:-robotiq_2f_85}
+
 python scripts/demo_object_pc.py \
     --sample_data_dir assets/sample_data/real_world \
-    --gripper_name robotiq_2f_85 \
+    --gripper_name "$GRIPPER_NAME" \
     --plot_top_mesh
